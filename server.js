3790d3de09681d783685f60589ee5de425c9aa95
Juncong Liang <juncongliang@Juncongs-MBP.fios-router.home>
2020-05-15 00:08:51 -0400

    still need to work on it

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const exphbs = require("express-handlebars");
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function () {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
